cd5f1df1058776b0a7f1a476c2b524d733b8680c
Darwin_N19 <darwinccn25@gmail.com>
2025-02-04 20:26:35 -0500

    remove useless file

--- apps/backend/src/database/columns.helper.ts
+++
@@ -1,6 +0,0 @@
-import { timestamp } from "drizzle-orm/pg-core";
-
-export const baseTimestamps = {
-  updated_at: timestamp().defaultNow().notNull(),
-  created_at: timestamp().defaultNow().notNull()
-}
