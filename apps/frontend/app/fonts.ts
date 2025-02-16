eb6f4f455230df4005b8edcb6c4da17928a9b419
Darwin_N19 <darwinccn25@gmail.com>
2025-02-16 00:23:38 -0500

    add integration with roboto font and migrate from tailwind v3 to v4

--- apps/frontend/app/fonts.ts
+++ apps/frontend/app/fonts.ts
@@ -1,52 +1,6 @@
-import localFont from "next/font/local";
+import { Roboto } from "next/font/google";
 
-export const calibreFont = localFont({
-	src: [
-		{
-			path: "./fonts/TestCalibre-Thin.otf",
-			weight: "300",
-			style: "normal",
-		},
-		{
-			path: "./fonts/TestCalibre-Thinit.otf",
-			weight: "300",
-			style: "italic",
-		},
-		{
-			path: "./fonts/TestCalibre-Regular.otf",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "./fonts/TestCalibre-Regularit.otf",
-			weight: "400",
-			style: "italic",
-		},
-		{
-			path: "./fonts/TestCalibre-Medium.otf",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "./fonts/TestCalibre-Mediumit.otf",
-			weight: "500",
-			style: "italic",
-		},
-		{
-			path: "./fonts/TestCalibre-Semibold.otf",
-			weight: "600",
-			style: "normal",
-		},
-		{
-			path: "./fonts/TestCalibre-Semiboldit.otf",
-			weight: "600",
-			style: "italic",
-		},
-		{
-			path: "./fonts/TestCalibre-Bold.otf",
-			weight: "700",
-			style: "normal",
-		},
-	],
-	variable: "--font-calibre",
+export const mainFont = Roboto({
+	weight: ["400", "500", "700"],
+	variable: "--font-roboto",
 });
